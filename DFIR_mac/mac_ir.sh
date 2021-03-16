175e938927f55c7a59b047a66663b8a994434a66
unknown <karas639@gmail.com>
2021-03-16 14:27:00 +0900

    update

--- DFIR_mac/mac_ir.sh
+++ DFIR_mac/mac_ir.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# privacy-i log path
+# 1. macOS agent install log
+# /usr/local/post_install.log
+# /usr/local/preinstall.log
+# 2. Privacy-i install path and data dir
+# /usr/local/Privacy-i
+# 3. Privacy-i web agent log
+# /usr/local/Privacy-i/discover/web/log/app.log
+# /usr/local/Privacy-i/discover/web/log/console.log
+# /usr/local/Privacy-i/discover/ProgramData/log/piagent.log
+# 4. Privacy-i login and server connect log
+# /usr/local/Privacy-i/discover/ProgramData/log/eventlog/*.dat
+
+# macOS System log
+
+
 tar cvf ~/Download/User-Library-Logs.tar ~/Library/Logs/
 tar cvf ~/Download/Library-Logs.tar /Library/Logs
 tar cvf ~/Download/var-log.tar /var/log/
